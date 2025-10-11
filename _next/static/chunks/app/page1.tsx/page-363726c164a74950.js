690e93974ecfae4d96e2712c2782f359a7e9fe79
Igor Correa Trifilio Campos <igorcorreatrifilioc@gmail.com>
2025-10-11 00:04:31 -0300

    Updates

---
+++ _next/static/chunks/app/page1.tsx/page-363726c164a74950.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[602],{9891:(e,s,_)=>{Promise.resolve().then(_.bind(_,1515))}},e=>{e.O(0,[524,544,619,566,441,255,358],()=>e(e.s=9891)),_N_E=e.O()}]);
--- _next/static/chunks/e707f2226c779fc4.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/f0b9dd61652cc1df.js",
-  "static/chunks/turbopack-93885bf65eadb4f3.js"
-])
